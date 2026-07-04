d033129dcdf92a7acac3c5ce4338dfa84a84c81e
omidard <omidard@biosustain.dtu.dk>
2026-07-04 14:57:14 +0000

    Media: label GrowthDB-added media as 'Literature (GrowthDB)' in source_db filter

--- build_index.py
+++ build_index.py
@@ -5,6 +5,7 @@ def source_db(idv, prov, food_group):
     if idv.startswith('usda_'): return 'USDA FoodData Central'
     if idv.startswith('food_'): return 'FooDB'
     if idv.startswith('lit_'): return 'Literature (GEM papers)'
+    if idv.startswith('growthlit_'): return 'Literature (GrowthDB)'
     if idv.startswith('biospecimen_hmdb_'): return 'HMDB'
     if idv.startswith('biospecimen_'): return 'Published (HMDB-derived)'
     return prov.get('source_type','')
